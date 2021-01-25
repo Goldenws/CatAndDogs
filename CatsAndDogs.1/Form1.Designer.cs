02e5bc5f6bdfbf45accea927b72691d2ace73f40
RolandStrod <KhaleAldeAsad@gmail.com>
2021-01-25 14:04:07 +0200

    CatsAndBoots

---
+++ CatsAndDogs.1/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿
+namespace CatsAndDogs._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.dogPicture = new System.Windows.Forms.PictureBox();
+            this.getTheDog = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // dogPicture
+            // 
+            this.dogPicture.Location = new System.Drawing.Point(878, 82);
+            this.dogPicture.Name = "dogPicture";
+            this.dogPicture.Size = new System.Drawing.Size(293, 287);
+            this.dogPicture.TabIndex = 1;
+            this.dogPicture.TabStop = false;
+            // 
+            // getTheDog
+            // 
+            this.getTheDog.Location = new System.Drawing.Point(916, 404);
+            this.getTheDog.Name = "getTheDog";
+            this.getTheDog.Size = new System.Drawing.Size(222, 33);
+            this.getTheDog.TabIndex = 2;
+            this.getTheDog.Text = "Get the dog Image";
+            this.getTheDog.UseVisualStyleBackColor = true;
+            this.getTheDog.Click += new System.EventHandler(this.getTheDog_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1282, 573);
+            this.Controls.Add(this.getTheDog);
+            this.Controls.Add(this.dogPicture);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dogPicture)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox dogPicture;
+        private System.Windows.Forms.Button getTheDog;
+    }
+}
+
